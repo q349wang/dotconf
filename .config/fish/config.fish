950935bead712c0bd9b7176f74f571f7a1c2a446
q349wang <lwang@live.ca>
2026-09-13 23:28:07 -0400

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+alias dotconf='/usr/bin/git --git-dir=/home/pyroc/.dotconf/ --work-tree=/home/pyroc'
